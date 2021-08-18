54fd107d9ed4b4dad4b500035a92e588aedfa8fe
Pranav Arya <43077165+PranavArya37@users.noreply.github.com>
2021-08-18 23:44:16 +0530

    Add files via upload

---
+++ Codes/CWH_04_literals.java
@@ -0,0 +1,15 @@
+/**
+ * CWH_04_literals
+ */
+public class CWH_04_literals {
+    public static void main(String[] args) {
+        byte age = 21;
+        int age2 = 56;
+        short age3 = 67;
+        long ageDino = 566666666666666l;
+        char ch = 'P';
+        float f1 = 5.6f;
+        double d1 = 4.66d;
+        System.out.println(age);
+    }
+}
